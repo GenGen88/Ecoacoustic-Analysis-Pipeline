37713680d1d6ff46216c3bdc99bec9877f5f4f02
Genna <g.l.dias@icloud.com>
2023-01-19 13:59:49 +1000

    One R graph

--- example/VRES_Genna.R
+++ example/VRES_Genna.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(dplyr)
 
-# import data
+# import data from VRES
 data1 <- read.csv("C:\\Users\\gldia\\OneDrive\\Documents\\VRES-Analyser\\example\\report.csv", header=TRUE, stringsAsFactors = FALSE)
+# import data from ubuntu thing
+data1 <- read.csv("C:\\Users\\gldia\\Documents\\report.csv",header=TRUE, stringsAsFactors = FALSE)
 
 # table of just species and time
 speciesTimeData <-data.frame(data1$CommonName, data1$date)
@@ -17,6 +19,10 @@ colnames(richness) = c("CommonName", "Frequency")
 #generates a table of species richness over date
 richnessDate <- speciesTimeData %>% group_by(data1$date)%>% count(data1$date)
 
-
-
-
+#graph
+ggplot(data=richnessDate, aes(x=data1$date, y=n))+
+  geom_point()+
+  theme_bw()+
+  ggtitle("Species Richness Over Time")
+  
+ 
