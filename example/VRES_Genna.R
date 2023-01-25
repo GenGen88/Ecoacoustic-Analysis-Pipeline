8c0dc45915c3f8dcb41de5ff6c76fde2a14a76fd
Genna <g.l.dias@icloud.com>
2023-01-26 00:29:54 +1000

    R

--- example/VRES_Genna.R
+++ example/VRES_Genna.R
@@ -9,9 +9,11 @@ filteredData <- filter(data1, data1$Confidence >0.5)
 
 # table of just species and time
 speciesTimeData <-data.frame(data1$CommonName, data1$date)
+colnames(speciesTimeData) = c("CommonName", "Date")
 
 # generates just the species name data
 speciesData <-data.frame(data1$CommonName)
+colnames(speciesData) = c("CommonName")
 
 #generates a table of how many times a species appeared
 richness <- speciesData %>% count(data1.CommonName)
@@ -27,10 +29,21 @@ ggplot(data=richnessDate, aes(x= Date, y = Richness))+
   theme_bw()+
   ggtitle("Species Richness Over Time")
   
-# graph richness over time (line)
+# graph richness over time (line??) for top 5?
 ggplot(data=richnessDate, aes(x= Date, y = Richness))+
+  geom_point()+
   geom_smooth()+
   theme_bw()+
   ggtitle("Species Richness Over Time")
 
- 
+ # biodiversity over time????
+n <- richness$Frequency
+N <- sum(richness$Frequency)
+parms <- c(n,N)
+calculate_biodiversity <- function(parms){
+  n <- parms[1]
+  N <- parms[2]
+  # function
+  biodiversity <- 1-(sum(n/N)^2)
+  return(biodiversity)
+}
