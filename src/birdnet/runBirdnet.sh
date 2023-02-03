6adea366609e082ddce434c99ec22b4317cce442
hudson-newey <neweyh@qut.edu.au>
2023-02-03 10:54:00 +1000

    removed check to remove duplicates

--- src/birdnet/runBirdnet.sh
+++ src/birdnet/runBirdnet.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# this line is needed for the case where the audio files are corrupted
-# in this case, birdnet results will not be produced. So if we don't delete previous results, the pipeline will use the last "good" results
-# therefore, causing duplicate results, and bad data
-if [ -f "./out/birdnet-results.csv" ]; then
-    rm $2;
-fi
-
 # in the case that the setup.sh script has not been run, the program should initilize birdnet
 if [ ! -d "./src/lib/BirdNET-Analyzer/" ]; then
     ./src/setup_lib.sh;
