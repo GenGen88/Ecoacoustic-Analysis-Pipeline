698dee00340654eccd23abda6943d0654122a369
hudson-newey <neweyh@qut.edu.au>
2023-02-03 10:55:02 +1000

    aded debug info

--- src/birdnet/runBirdnet.sh
+++ src/birdnet/runBirdnet.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+# this line is needed for the case where the audio files are corrupted
+# in this case, birdnet results will not be produced. So if we don't delete previous results, the pipeline will use the last "good" results
+# therefore, causing duplicate results, and bad data
+if [ -f "./out/birdnet-results.csv" ]; then
+    rm $2;
+fi
+
 # in the case that the setup.sh script has not been run, the program should initilize birdnet
 if [ ! -d "./src/lib/BirdNET-Analyzer/" ]; then
     ./src/setup_lib.sh;
 fi
+
+echo "In file";
+echo $1;
+
+echo -e "\nout file";
+echo $2;
 
 # TODO: this should check if the user is using python or python3
 python3 ./lib/BirdNET-Analyzer/analyze.py --i $1 --o $2;
