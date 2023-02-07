b3531312c38ea49a5fdaaca2300e5d0b19d3327f
hudson-newey <neweyh@qut.edu.au>
2023-02-07 10:23:38 +1000

    added anova test

--- src/analysis/scripts/biodiversity_significance.r
+++ src/analysis/scripts/biodiversity_significance.r
@@ -13,7 +13,7 @@ csv_in <- csv_in %>% filter(!SpeciesCode %in% excluded_results)
 
 # since BirdNet logs results with accuracy < 0.5, we need to discard these results
 # season data is also incorrect, so extract this
-df <- csv_in %>% filter(Confidence >= 0.8) %>% subset(select = -season)
+df <- csv_in %>% filter(Confidence >= 0.6) %>% subset(select = -season)
 
 # add seasonal information to tibble
 df <- df %>% mutate(month = 
@@ -35,19 +35,19 @@ ev <- df %>% mutate(heat =
 # calculate the biodiversity of wet vs dry
 dry_winter_detections <- ev %>% filter(heat == 1 & isWet == F)
 dry_winter_richness <- dry_winter_detections %>% select(SpeciesCode) %>% unique() %>% count()
-dry_winter_biodiversity <- dry_winter_richness / (dry_winter_detections %>% count())
+dry_winter_biodiversity <- as.numeric(dry_winter_richness / (dry_winter_detections %>% count()))
 
 dry_summer_detections <- ev %>% filter(heat == 3 & isWet == F)
 dry_summer_richess <- dry_summer_detections %>% select(SpeciesCode) %>% unique() %>% count()
-dry_summer_biodiversity <- dry_summer_richess / (dry_summer_detections %>% count())
+dry_summer_biodiversity <- as.numeric(dry_summer_richess / (dry_summer_detections %>% count()))
 
 wet_winter_detections <- ev %>% filter(heat == 1 & isWet == T)
 wet_winter_richness <- wet_winter_detections %>% select(SpeciesCode) %>% unique() %>% count()
-wet_winter_biodiversity <- wet_winter_richness / (wet_winter_detections %>% count())
+wet_winter_biodiversity <- as.numeric(wet_winter_richness / (wet_winter_detections %>% count()))
 
 wet_summer_detections <- ev %>% filter(heat == 3 & isWet == T)
 wet_summer_richness <- wet_summer_detections %>% select(SpeciesCode) %>% unique() %>% count()
-wet_summer_biodiversity <- wet_summer_richness / (wet_summer_detections %>% count())
+wet_summer_biodiversity <- as.numeric(wet_summer_richness / (wet_summer_detections %>% count()))
 
 # just seasons biodiversity (both wet + dry)
 summer_detections <- ev %>%filter(heat == 3)
@@ -67,9 +67,16 @@ results <- bind_rows(
 
 # rejection region |Z| > 1.96
 
-wet <- c(21, 50)
-dry <- c(63, 67)
+biodiversity <- c(dry_summer_biodiversity, dry_winter_biodiversity, wet_summer_biodiversity, wet_winter_biodiversity)
+wet_dry <- c("dry", "dry", "wet", "wet")
+season <- c("summer", "winter", "summer", "winter")
 
-df <- tibble(wet,dry)
+dry <- c(dry_summer_biodiversity, dry_winter_biodiversity)
+wet <- c(wet_summer_biodiversity, wet_winter_biodiversity)
 
-t.test(df) %>% pander()
+df <- tibble(wet_dry, season, biodiversity)
+
+# using an anova table to measure the variability within variables
+aov(biodiversity ~ wet_dry + season, data = df) %>% pander()
+
+glm(biodiversity ~ wet_dry + season, data = df) %>% pander()
