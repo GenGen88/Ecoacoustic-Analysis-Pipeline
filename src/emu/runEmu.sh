3ab624888d8842938b3bc6cd6bf992da3d009eeb
hudson-newey <neweyh@qut.edu.au>
2023-02-09 22:43:32 +1000

    modified emu to fail gracefully

--- src/emu/runEmu.sh
+++ src/emu/runEmu.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-emu fix apply --all $1;
+
+# if emu is not installed, we want to fail gracefully
+which emu;
+if [ $? -eq 0 ]; then
+    emu fix apply --all $1;
+else
+    echo "Warning: EMU is not installed";
+    echo -e "\tAny bugs in audio files will not fixed and will be reported as corrupted"
+    echo -e "\tPlease install emu from https://github.com/QutEcoacoustics/emu"
+fi
