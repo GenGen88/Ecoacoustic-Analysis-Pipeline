80079b9e577da66a5e056a99f070abe3c7ae95c2
hudson-newey <neweyh@qut.edu.au>
2023-02-09 22:47:10 +1000

    changed system calls to use helper function

--- src/birdnet/birdNetAnalyses.py
+++ src/birdnet/birdNetAnalyses.py
@@ -1,6 +1,7 @@
 import os
 from util.constants import DIR_BIRDNET_OUT_FILE_PATH
+from util.util import runCommand
 
 def runBirdNet(fileName: str) -> None:
     print(f"\nRunning analysis on {fileName}, printing results to {DIR_BIRDNET_OUT_FILE_PATH}")
-    os.system(f"./src/birdnet/runBirdnet.sh {fileName} {DIR_BIRDNET_OUT_FILE_PATH}")
+    runCommand(f"./src/birdnet/runBirdnet.sh {fileName} {DIR_BIRDNET_OUT_FILE_PATH}")
--- src/environmentVariables/weatherData/parseCSV.py
+++ src/environmentVariables/weatherData/parseCSV.py
@@ -1,4 +1,4 @@
-from util.util import deleteFile, isFolder, throwError, fileExists, directoryFiles, readFile, writeToFile
+from util.util import deleteFile, isFolder, throwError, fileExists, directoryFiles, readFile, writeToFile, runCommand
 from environmentVariables.weatherData.constants import WEATHER_DATA_START_INDEX, WEATHER_FETCHER_SCRIPT_PATH
 from util.constants import DIR_WEATHER_IN_FILE_PATH
 
@@ -8,8 +8,7 @@ import os
 def aggregateCSVFiles(inPath: str, outPath: str = "./out/weatherData.csv") -> str:
     if not isFolder(inPath):
         throwError(f"No input data for weather data declared.\nPlease place weather data in {DIR_WEATHER_IN_FILE_PATH}", fatal=False, errorCode=404)
-        # # TODO: Genna is working on the fetcher for data
-        os.system(WEATHER_FETCHER_SCRIPT_PATH)
+        runCommand(WEATHER_FETCHER_SCRIPT_PATH)
 
     # remove results from previous runs
     if fileExists(outPath):
