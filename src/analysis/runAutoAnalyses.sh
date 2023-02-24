6d769431190c4ac20dc3f182cf3a1c194c27eb98
hudson-newey <neweyh@qut.edu.au>
2023-02-24 22:19:36 +1000

    modified rscripts to run as bash daemon

--- src/analysis/runAutoAnalyses.sh
+++ src/analysis/runAutoAnalyses.sh
@@ -5,5 +5,7 @@ do
     # R scripts should follow the metadata standard defined in
     # https://github.com/ecoacoustics/metadata-standard/blob/master/cli.md
     # however, if the rscript is not expecting any CLA's, they should be ignored and not cause any breakage
-    Rscript $file --output $1 $2
+
+    # the STD-IO pipes are used to run the R-Script as a Daemon, allowing for native BASH parelell processing
+    Rscript $file --output $1 $2 >/dev/null 2>&1 < /dev/null &;
 done
